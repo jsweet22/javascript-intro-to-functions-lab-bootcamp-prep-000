8574b0137ce19e368301a9ae510ab1ed53bc7263
Janae Sweet <janaemarie_@hotmail.com>
2018-06-04 18:46:02 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,37 @@
+function shout(string) {
+  return string.toUpperCase()
+}
+
+function whisper(string) {
+  return string.toLowerCase()
+}
+
+
+function logShout(string) {
+  console.log(`${string.toUpperCase()}`)
+  return `${string.toUpperCase() }`
+}
+
+function logWhisper(string) {
+  console.log(`${string.toLowerCase()}`)
+  return `${string.toUpperCase() }`
+}
+
+function sayHiToGrandma(string) {
+  var uppercase = "YES INDEED!"
+  var lowercase = "I can't hear you!"
+  var love = "I love you, Grandma."
+  
+  if ( lowercase.toUpperCase() === lower) {
+	  return "I can't hear you!"; }
+}
+
+function sayHiToGrandma(string) {
+  if ( uppercase.toUpperCase() === string) {
+    return 'YES INDEED!' }
+}
+
+function sayHiToGrandma(string) {
+  if (string === 'I love you, Grandma.') {
+    return ('I love you, too.') }
+}
